caf3d7ec2641e0f1f346d31db8d8ed1c8b8eaae5
Maxime Moreillon <m.moreillon@gmail.com>
2023-01-27 07:22:15 +0900

    readme

--- README.md
+++ README.md
@@ -2,12 +2,48 @@
 
 An express middleware that generates routes and CRUD controllers for every table defined in a prisma schema.
 
+## Install
+
+This module can be installed using NPM:
+
+```
+npm install @moreillon/prisma-auto-crud
+```
+
+## Usage
+
+This module is intended to be used as an Express middleware.
+
+```
+const express = require( 'express')
+const prismaAutoCrud = require( './index.js')
+const { PrismaClient } = require('@prisma/client')
+
+const prismaClient = new PrismaClient()
+
+const {
+    PORT = 7070
+} = process.env
+
+
+const app = express()
+app.use(express.json())
+
+app.use(prismaAutoCrud(prismaClient))
+
+
+app.listen(PORT, () => {
+    console.log(`[Express] Listening on port ${PORT}`)
+})
+```
+
 ## Development notes
- * using [Prisma generators](https://github.com/YassinEldeeb/create-prisma-generator) could be a better approach 
 
-## To do:
+- using [Prisma generators](https://github.com/YassinEldeeb/create-prisma-generator) could be a better approach
 
-* [x] Pagination
-* [ ] Sorting, ordering
-* [ ] Query filters
-* And more...
+### To do:
+
+- [x] Pagination
+- [x] Sorting, ordering
+- [x] Query filters
+- And more...
--- factories/crud.js
+++ factories/crud.js
@@ -1,106 +1,86 @@
-
 exports.genrateItemCreate = (prismaTableController) => {
-
-    return async (req, res, next) => {
-
-        try {
-            const data = req.body
-            const newItem = await prismaTableController.create({ data })
-            res.send(newItem)
-        } catch (error) {
-            next(error)
-        }
-
+  return async (req, res, next) => {
+    try {
+      const data = req.body
+      const newItem = await prismaTableController.create({ data })
+      res.send(newItem)
+    } catch (error) {
+      next(error)
     }
+  }
 }
 
 exports.genrateItemsRead = (prismaTableController) => {
+  return async (req, res, next) => {
+    try {
+      const {
+        skip = 0,
+        take = 100,
+        sort = "id",
+        order = "desc",
+        ...rest
+      } = req.query
 
-    return async (req, res, next) => {
+      // Note: offset pagination does not scale well
+      // https://www.prisma.io/docs/concepts/components/prisma-client/pagination#-cons-of-offset-pagination
+      const query = {
+        skip: Number(skip),
+        take: Number(take),
+        orderBy: [{ [sort]: order }],
+        where: { ...rest },
+      }
 
-        try {
+      const items = await prismaTableController.findMany(query)
 
-            // TODO: Sorting and ordering
-            const {
-                skip = 0,
-                take = 100,
-                sort = 'id',
-                order = 'desc',
-                ...rest
-            } = req.query
+      const total = await prismaTableController.count({ where: { ...rest } })
 
-            // Note: offset pagination does not scale well
-            // https://www.prisma.io/docs/concepts/components/prisma-client/pagination#-cons-of-offset-pagination
-            const query = {
-                skip: Number(skip), 
-                take: Number(take), 
-                orderBy: [ {[sort]: order} ],
-                where: {...rest}
-            }
-
-            const items = await prismaTableController.findMany(query)
-
-            const total = await prismaTableController.count({ where: { ...rest } })
-
-            res.send({skip, take, total, items})
-        } catch (error) {
-            next(error)
-        }
-
+      res.send({ skip, take, total, items })
+    } catch (error) {
+      next(error)
     }
+  }
 }
 
 exports.genrateItemRead = (prismaTableController) => {
+  // TODO: allow user to do includes
 
-    // TODO: allow user to do includes
-
-    return async (req, res, next) => {
-
-        try {
-            const { id } = req.params
-            const query = { where: { id: Number(id) } }
-            const item = await prismaTableController.findUnique(query)
-            res.send(item)
-        } catch (error) {
-            next(error)
-        }
-
+  return async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const query = { where: { id: Number(id) } }
+      const item = await prismaTableController.findUnique(query)
+      res.send(item)
+    } catch (error) {
+      next(error)
     }
+  }
 }
-
 
 exports.genrateItemUpdate = (prismaTableController) => {
+  return async (req, res, next) => {
+    try {
+      const data = req.body
+      const { id } = req.params
 
-    return async (req, res, next) => {
+      const query = { where: { id: Number(id) }, data }
 
-        try {
-            const data = req.body
-            const { id } = req.params
-
-            const query = { where: { id: Number(id) }, data }
-
-            const itemUpdate = await prismaTableController.update(query)
-            res.send(itemUpdate)
-        } catch (error) {
-            next(error)
-        }
-
+      const itemUpdate = await prismaTableController.update(query)
+      res.send(itemUpdate)
+    } catch (error) {
+      next(error)
     }
+  }
 }
 
-
 exports.genrateItemDelete = (prismaTableController) => {
-
-    return async (req, res, next) => {
-
-        try {
-            const { id } = req.params
-            const query = { where: { id: Number(id) } }
-            const itemDelete = await prismaTableController.delete(query)
-            res.send(itemDelete)
-        } catch (error) {
-            next(error)
-        }
-
+  return async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const query = { where: { id: Number(id) } }
+      const itemDelete = await prismaTableController.delete(query)
+      res.send(itemDelete)
+    } catch (error) {
+      next(error)
     }
+  }
 }
