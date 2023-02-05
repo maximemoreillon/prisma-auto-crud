7ebd373e874c33441c046c38b4d2658c160d617e
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-05 12:01:39 +0900

    docker container

--- index.js
+++ index.js
@@ -1,44 +1,41 @@
-const { Router } = require('express')
-const { generateTableRouter } = require( './factories/router.js')
-const createHttpError = require( 'http-errors')
+// This file is the middleware
+const { Router } = require("express")
+const { generateTableRouter } = require("./factories/router.js")
+const createHttpError = require("http-errors")
 
 // Note: Middleware cannot be async
 module.exports = (prismaClient, opts = {}) => {
+  const router = Router()
 
-    const router = Router()
+  prismaClient._getDmmf().then((dmmf) => {
+    const models = dmmf.datamodel.models
+    const tables = models.map((model) => model.name)
 
-    prismaClient._getDmmf()
-        .then(dmmf => {
+    router.get("/models", (req, res) => {
+      res.send(models)
+    })
 
+    router.get("/models/:modelName", (req, res) => {
+      const { modelName } = req.params
+      const model = dmmf.modelMap[modelName]
+      if (!model)
+        throw createHttpError(404, `Model ${modelName} does not exist`)
+      res.send(model)
+    })
 
-            const models = dmmf.datamodel.models
-            const tables = models.map(model => model.name)
+    router.get("/tables", (req, res) => {
+      // Redundant with above
+      res.send(tables)
+    })
 
-            router.get('/models', (req, res) => {
-                res.send(models)
-            })
+    // Generate routes and their controllers for each table
+    tables.forEach((tableName) => {
+      const tableRoute = `/${tableName}`
+      const prismaTableController = prismaClient[tableName]
+      const tableRouter = generateTableRouter(prismaTableController)
+      router.use(tableRoute, tableRouter)
+    })
+  })
 
-            router.get('/models/:modelName', (req, res) => {
-                const { modelName } = req.params
-                const model = dmmf.modelMap[modelName]
-                if (!model) throw createHttpError(404, `Model ${modelName} does not exist`)
-                res.send(model)
-            })
-
-            router.get('/tables', (req, res) => {
-                // Redundant with above
-                res.send(tables)
-            })
-
-            // Generate routes and their controllers for each table
-            tables.forEach(tableName => {
-                const tableRoute = `/${tableName}`
-                const prismaTableController = prismaClient[tableName]
-                const tableRouter = generateTableRouter(prismaTableController)
-                router.use(tableRoute, tableRouter)
-
-            })
-        })
-    
-    return router
+  return router
 }
---
+++ server.js
@@ -0,0 +1,24 @@
+// This file is used in the Docker container
+
+const express = require("express")
+const cors = require("cors")
+const prismaAutoCrud = require("./index.js")
+const prismaClient = require("./prismaClient.js")
+
+const { PORT = 80 } = process.env
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send({
+    application_name: "Prisma Auto CRUD",
+  })
+})
+
+app.use(prismaAutoCrud(prismaClient))
+
+app.listen(PORT, () => {
+  console.log(`[Express] Listening on port ${PORT}`)
+})
