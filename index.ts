d6f13dd1faeb1e3a3488515b042917dbd88ce5ac
Maxime Moreillon <m.moreillon@gmail.com>
2023-10-30 07:32:56 +0900

    prisma v5

--- index.ts
+++ index.ts
@@ -2,45 +2,43 @@
 import { Router, Request, Response } from "express"
 import { generateTableRouter } from "./factories/router"
 import createHttpError from "http-errors"
+import { PrismaClient } from "@prisma/client"
 
 // Note: Middleware cannot be async
-// TODO: find types
-const middleware = (prismaClient: any, opts = {}) => {
+const middleware = (prismaClient: PrismaClient, opts = {}) => {
   const router = Router()
 
-  prismaClient._getDmmf().then((dmmf: any) => {
-    const models = dmmf.datamodel.models
+  // @ts-ignore
+  const modelsMap = prismaClient._runtimeDataModel.models
+  const modelNames = Object.keys(modelsMap)
 
-    router.get("/models", (req: Request, res: Response) => {
-      res.send(models)
-    })
+  router.get("/models", (req: Request, res: Response) => {
+    res.send(modelNames)
+  })
 
-    router.get("/models/:modelName", (req: Request, res: Response) => {
-      const { modelName } = req.params
-      const model = dmmf.modelMap[modelName]
-      if (!model)
-        throw createHttpError(404, `Model ${modelName} does not exist`)
-      res.send(model)
-    })
+  router.get("/models/:modelName", (req: Request, res: Response) => {
+    const { modelName } = req.params
+    const model = modelsMap[modelName]
+    if (!model) throw createHttpError(404, `Model ${modelName} does not exist`)
+    res.send(model)
+  })
 
-    // Generate routes and their controllers for each table
-    models.forEach((model: any) => {
-      const { name, fields } = model
-      const tableRoute = `/${name}`
-      const prismaTableController = prismaClient[name]
+  // Generate routes and their controllers for each table
+  modelNames.forEach((name: any) => {
+    const { fields } = modelsMap[name]
+    const tableRoute = `/${name}`
+    const prismaTableController: any = prismaClient[name]
 
-      // Adding the field used as id
-      prismaTableController.primaryKeyField = fields.find(
-        ({ isId }: any) => isId
-      )?.name
+    // Adding the field used as id
+    prismaTableController.primaryKeyField = fields.find(
+      ({ isId }: any) => isId
+    )?.name
 
-      const tableRouter = generateTableRouter(prismaTableController)
-      router.use(tableRoute, tableRouter)
-    })
+    const tableRouter = generateTableRouter(prismaTableController)
+    router.use(tableRoute, tableRouter)
   })
 
   return router
 }
 
-// export = middleware
 export default middleware
